c31a0cb6732bf0207f00de2175dd2c197234fbc9
Dokjolly0 <alexviolattolibero.it@gmail.com>
2025-07-14 21:23:34 +0200

    Add link Backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://miodominio.it/api',
+  apiUrl: 'https://its-exam-backend.onrender.com/api',
   googleAuthLink: 'https://miodominio.it/api/auth/google',
   gitHubAuthLink: 'https://miodominio.it/api/auth/github',
 };
